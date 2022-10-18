0fb9d9e3775c5a92e8ae2fb29e632ccace5f0b69
Grover101 <limachigrover09@gmail.com>
2022-10-18 19:28:10 -0400

    add: metodo para crear el dataset

---
+++ dataset.py
@@ -0,0 +1,51 @@
+import math
+import os
+import shutil
+
+
+def creacion_dataset():
+    try:
+        route_root = os.getcwd()
+        route_dataset = route_root+'/dataset'
+        images_in_folder_root = os.path.join(route_root+'/img')
+        if ('dataset' not in os.listdir()):
+            os.makedirs('dataset')
+
+        # Obtener las clases de las poses
+        folder_class = sorted(
+            [n for n in os.listdir(images_in_folder_root)]
+        )
+
+        for pose_class_name in folder_class:
+            # ruta para las clases de pose
+            images_in_folder = os.path.join(
+                images_in_folder_root, pose_class_name)
+
+            # Numero de imagenes por clase
+            list_image = os.listdir(images_in_folder)
+            num_image = len(list_image)
+            # print(pose_class_name, num_image)
+
+            # 80% de las imagenes para entrenaminto
+            train_image = math.trunc(num_image * 0.8)
+            # print('train', pose_class_name, train_image)
+
+            # Creando el dataset para train y test
+            route = os.path.join(route_dataset, 'train', pose_class_name)
+            os.makedirs(route, exist_ok=True)
+            for index, image in enumerate(list_image):
+                if train_image == index+1:
+                    route = os.path.join(
+                        route_dataset, 'test', pose_class_name)
+                    os.makedirs(route, exist_ok=True)
+                src = os.path.join(images_in_folder, image)  # origen
+                dst = os.path.join(route, image)  # destino
+                shutil.copyfile(src, dst)
+
+        print('Dataset creado 👻👌')
+    except:
+        print('Ocurrio un problema al crear el dataset')
+
+
+if __name__ == '__main__':
+    creacion_dataset()
